3203a350b356d6f7a3870abea954a3bcd06c2d90
maokaiqi <31732521@qq.com>
2016-12-21 23:58:04 +0800

    添加ex9

---
+++ ex9.py
@@ -0,0 +1,19 @@
+#coding:utf-8
+#Here's some new strange stuff, rember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# \n是换行符， \是反斜杠 在回车键的上方，  另一个/和问号一个键
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+# ”“”
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5,or 6.
+"""
+
+
