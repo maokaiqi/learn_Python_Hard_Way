65e95eac7f4ef2170d0c75bb6522e304369c389b
maokaiqi <31732521@qq.com>
2016-12-24 01:36:47 +0800

    add ex21

--- ex20.py
+++ ex20.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from sys import argv
 
 script, input_file = argv
@@ -21,7 +22,7 @@ print "Now let's rewind, kind of like a tape."
 rewind(current_file)
 
 print "Let's print three lines:"
-
+#由于seek指针回到了整文的初始位置，所以读取每一行是从第一行开始，读取之后指针到了下一行的起始位置。接着再次读取就从第二行开始，依此类推；
 current_line = 1
 print_a_line(current_line, current_file)
 
---
+++ ex21_1.py
@@ -0,0 +1,12 @@
+def re(a,b):
+	return 32
+
+result1 = re(2,3)
+
+def r1(a,b,c):
+	return 1+a+b+c
+
+result2 = r1(2,3,4)
+
+print result1,result2
+
